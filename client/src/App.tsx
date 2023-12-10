d55259ed2ad7aa124bab769b6e02b2fc2a488b3f
keenanlk <keenanlkaufman@gmail.com>
2023-12-10 11:16:45 -0600

    chore: initialize react app

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="display-flex justify-center items-center flex-1">
+      <h1 className="text-red-500">Spotify to YouTube Converter</h1>
+    </div>
+  );
+}
+
+export default App;
